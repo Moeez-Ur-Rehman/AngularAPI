5e49a85238f4e65836e9b034980c3f7a5bee7a20
unknown <moiz1rehman@gmail.com>
2024-09-06 16:37:56 +0500

    This is a new commit for what I originally planned to be amended

---
+++ Serve.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/helloDB', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB', err));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello World from the API!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
